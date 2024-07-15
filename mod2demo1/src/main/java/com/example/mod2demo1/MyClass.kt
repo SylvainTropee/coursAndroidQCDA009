adaa4f095358dbf62ccbdf9f05a40e9de94cd45a
stropee <stropee@campus-eni.fr>
2024-07-15 14:59:48 +0200

    mod2demo1

---
+++ mod2demo1/src/main/java/com/example/mod2demo1/MyClass.kt
@@ -0,0 +1,38 @@
+package com.example.mod2demo1
+
+//fun main() {
+//    val message = "Hello World !"
+//    println(message)
+//}
+
+////différence entre val et var
+//fun main() {
+//
+//    //typage non obligatoire, car inférrence de type
+//    val value : Int  = 10;
+//    //value = 20; //pas possible avec val
+//
+//    //possibilité de modifier la donnée mais pas son type
+//    var value2 = 50
+//    value2 = 150
+//}
+
+//fun main() {
+//    var word = "World !"
+//    val message by lazy { "Hello $word" }
+//
+//   // println(message)
+//    word = "Man !"
+//
+//    println(message)
+//}
+
+
+fun main() {
+    var word : String? = null
+    println(word)
+    word = "coucou"
+}
+
+
+
